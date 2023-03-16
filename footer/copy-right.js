5c267c3c3701e48af0e833625deaea8f6a2ac379
kh1012 <kh1012@midasit.com>
2023-03-16 11:10:23 +0900

    test

---
+++ footer/copy-right.js
@@ -0,0 +1,9 @@
+const footer = document.getElementById('footer-copyright');
+footer.innerHTML = "Copyright © SINCE 1989<br />MIDAS Information Technology Co., Ltd.<br />All rights reserved.";
+
+footer.style.color = '#fff';
+footer.style.fontSize = '1.25rem';
+footer.style.lineHeight = '2.5rem';
+footer.style.fontWeight = '700';
+footer.style.textAlign = 'center';
+footer.style.padding = '1rem 0rem 2rem 0rem';
