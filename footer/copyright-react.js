a0a1843e759064b9e3ea7fa3b52c9ca31d1607d9
kh1012 <kh1012@midasit.com>
2023-03-16 11:34:55 +0900

    seperate copyright html-react

---
+++ footer/copyright-react.js
@@ -0,0 +1,14 @@
+export function FooterCopyright() {
+  return (
+    <footer style={{
+      color: '#ffffff',
+      fontSize: '1.25rem',
+      lineHeight: '2rem',
+      fontWeight: '700',
+      textAlign: 'center',
+      padding: '1rem 0rem 2rem 0rem',
+    }}>
+      Copyright © SINCE 1989<br />MIDAS Information Technology Co., Ltd.<br />All rights reserved.
+    </footer>
+  );
+}
